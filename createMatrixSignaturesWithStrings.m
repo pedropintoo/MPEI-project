de5e97892bc29837142536ea851ea96eaf2ad6b3
JorgeGuiCDomingues <jguidomingues@gmail.com>
2023-12-22 10:43:17 +0000

    matrix Shingles

--- createMatrixSignaturesWithStrings.m
+++ createMatrixSignaturesWithStrings.m
@@ -14,7 +14,7 @@ function MatrixSigShingles = createMatrixSignaturesWithStrings(stringSets, k)
                 key = [x(sh) num2str(fh)];
                 hc = string2hash(key);
                 hc = mod(hc, length(MatrixSigShingles)) + 1;
-                MatrixSigShingles(fh,u) = min(hc);
+                MatrixSigShingles(fh,u) = min(hc, MatrixSigShingles(fh, u));
             end
         end
     end
